d7f30979e33e1a13bafca87d8cb51a91defd690b
Vishakha Sahu <vishakhasahu023@gmail.com>
2023-02-17 23:20:21 +0530

    v - 6 About Page

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title' >About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        Duis aute irure dolor in reprehenderit in voluptate.
+        Excepteur sint occaecat cupidatat non proident.
+      </p>
+    </section>
+  )
+}
+
+export default About
